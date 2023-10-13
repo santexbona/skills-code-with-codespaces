7d7588ea8024eb367807eaa49227998c87498db1
Ikyohia Segun Bonaventure <69623338+santexbona@users.noreply.github.com>
2023-10-13 08:35:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var memberButton = document.getElementById("memberButton");
+    var skillsButton = document.getElementById("skillsButton");
+    member.style.display = "block";
+    skills.style.display = "none";
+    memberButton.style.backgroundColor = "#f1f1f1";
+    skillsButton.style.backgroundColor = "#ccc";
+}    
